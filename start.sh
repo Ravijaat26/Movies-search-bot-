7bc9e36ce8fc431a97375ff0a112ac8135ce7ae7
Ravijaat26 <119647661+Ravijaat26@users.noreply.github.com>
2022-12-21 21:08:41 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/Cyniteofficial/MdiskSearchBotV2.git /MdiskSearchBotV2
-cd /MdiskSearchBotV2
+git clone https://github.com/Ravijaat26/Movies-search-bot-.git /Movies-search-bot-
+cd /Movies-search-bot-
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
